69c0ad4331babc7b073a5cde5812ae7881573a00
xbw <windworst@gmail.com>
2018-05-29 23:17:30 +0800

    remove docker build '-q' arg

--- wrapper.bat
+++ wrapper.bat
@@ -4,5 +4,5 @@ set DIR=%DIR:\=/%
 set DIR=%DIR::=%
 set IMAGE=docker-bundle
 echo Enter Docker env...may wait...................
-docker build "%~dp0." -q -f "%~dp0Dockerfile.w" -t %IMAGE%
+docker build "%~dp0." -f "%~dp0Dockerfile.w" -t %IMAGE%
 docker run --rm -it -v //var/run/docker.sock:/var/run/docker.sock -v "%DIR%:%DIR%" -w "%DIR%" %IMAGE% %*
